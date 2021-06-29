33e62cda79bd5b1222029b00b8a19e8305988402
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-06-28 23:11:43 -0700

    add UIA2 and Flutter cases

--- appium-test/main_test.dart
+++ appium-test/main_test.dart
@@ -1,4 +1,6 @@
 import 'package:appium_driver/async_io.dart';
+import 'package:appium_driver/src/common/flutter_finder.dart';
+
 import 'package:test/test.dart';
 
 void main() {
@@ -8,12 +10,15 @@ void main() {
     driver = await createDriver(
         uri: Uri.parse('http://127.0.0.1:4723/wd/hub/'),
         desired: {
-    'platformName': 'android',
-    'automationName': 'uiautomator2',
-    'app': 'https://github.com/KazuCocoa/my_flutter_app3/blob/no-flutter-driver/apps/android-release-without-flutter-d/app-release.apk?raw=true',
-    'udid': 'D0AA002182JC0202126',
-    'packageName': 'com.example.my_flutter_app3'
-    });
+          'platformName': 'android',
+          'appium:automationName': 'uiautomator2',
+          // 'appium:automationName': 'flutter',
+          'appium:app': 'https://github.com/KazuCocoa/example-appium-flutter-dart/blob/main/apps/app-release-with-key-no-ext.apk?raw=true',
+          // 'appium:app': 'https://github.com/KazuCocoa/example-appium-flutter-dart/blob/main/apps/app-debug-with-ext-key.apk?raw=true',
+          'appium:udid': 'D0AA002182JC0202126',
+          'appium:appPackage': 'com.example.appium_flutter_webinar',
+          'appium:fullReset': true,  // to ensure the device under test clean the app under test.
+        });
   });
 
   tearDownAll(() async {
@@ -21,13 +26,28 @@ void main() {
   });
 
   test('click increment twice', () async {
+    // UIA2
     var incrementElement = await driver.findElement(
         AppiumBy.accessibilityId('Increment'));
 
-    await incrementElement.click();
-    await incrementElement.click();
+    // Flutter
+    // var incrementFinder = AppiumFlutterFinder.byKeyValueString('increment');
+    // var incrementElement = driver.getElement(incrementFinder);
 
-    var count = driver.findElements(AppiumBy.accessibilityId('2'));
-    expect(await count.length, 1);
+    for (var i = 0; i < 10; i++ ) {
+      await incrementElement.click();
+    }
+
+    try {
+      // UIA2
+      await driver.findElement(AppiumBy.accessibilityId('10'));
+
+      // Flutter
+      // var countFinder = AppiumFlutterFinder.bySemanticsLabel('10');
+      // driver.getElement(countFinder);
+      assert(true);
+    } catch (NoSuchElementException) {
+      assert(false);
+    }
   });
 }
